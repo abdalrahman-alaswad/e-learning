65b90520c763d7017b4fc52fe40638f28bbcfc87
abdalrahman alaswad <126125757+abdalrahman-alaswad@users.noreply.github.com>
2023-06-18 06:06:01 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import PrivateRoutesAdmin from './utlis/PrivateRoutesAdmin';
 function App() {
   return (
     <div className="App">
-    'd,as';,das;'
       <Router>
         {/* <Header /> */}
         <Container >
